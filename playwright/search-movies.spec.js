969f9127649aa84e8ba75fe951761ffb8ce364f6
mikael-eriksson1 <mikael.a.eriksson@skf.com>
2022-11-23 09:37:23 +0100

    Update

--- playwright/search-movies.spec.js
+++ playwright/search-movies.spec.js
@@ -4,15 +4,12 @@ import { test, expect } from '@playwright/test';
 test('shows movies in a list', async ({ page }) => {
   page.goto('/');
 
-  await page.getByPlaceholder('Enter search word').click();
-  await page.getByPlaceholder('Enter search word').fill('godfather');
-  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: /search/i }).fill('godfather');
+  await page.getByRole('button', { name: /search/i }).click();
 
   await expect(page.getByRole('listitem')).toContainText([
     'The Godfather',
     'The Godfather Part II',
     'The Godfather Part III',
   ]);
-
-  await page.pause();
 });
